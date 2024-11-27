b69efa229c6e720c92e515bd7bdeec54805092ac
mdsaiful170 <saifulsylhet00@gmail.com>
2024-11-27 15:19:17 +0600

    effort data set done

---
+++ src/component/page/Effort.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Containerbox from '../material/Containerbox'
+import { effortData } from '../../../lib/data'
+
+
+
+const Effort = () => {
+  return (
+    <>
+      <section className='px-3'>
+        <Containerbox className={"bg-[url(./effortbg.svg)] object-cover bg-center bg-no-repeat w-full"}>
+            <div>
+                <div>
+                    <h3></h3>
+                </div>
+            </div>
+        </Containerbox>
+      </section>
+    </>
+  )
+}
+
+export default Effort
